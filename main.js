92e726f61eb9653c6fa6a49601437ccb582afbcb
Dream <drimonis@protonmail.com>
2025-02-26 18:11:57 +0200

    another check point

--- main.js
+++ main.js
@@ -1,36 +1,37 @@
 let humanScore = 0;
 let computerScore = 0;
 
-
 // COMPUTER CHOICE
 function getComputerChoice() {
- 
-  
- const randomNumber = Math.floor(Math.random() * 3);
- 
+  const randomNumber = Math.floor(Math.random() * 3);
+
   if (randomNumber === 0) {
-    return 'Rock';
+    return "rock";
   } else if (randomNumber === 1) {
-    return 'Paper';
+    return "paper";
   } else {
-    return 'Scissors';
+    return "scissors";
   }
 }
 
 const computerChoice = getComputerChoice();
 console.log(computerChoice);
 
-
 // HUMAN CHOICE
 
 function getHumanChoice() {
-  
-  const humanChoice = prompt('Choose between Rock, Paper, Scissors').toLocaleLowerCase();  //PROMPT FOR HUMAN CHOICE and toLowerCase
- 
-  if (humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') {
+  const humanChoice = prompt(
+    "Choose between Rock, Paper, Scissors"
+  ).toLocaleLowerCase(); //PROMPT FOR HUMAN CHOICE and toLowerCase
+
+  if (
+    humanChoice === "rock" ||
+    humanChoice === "paper" ||
+    humanChoice === "scissors"
+  ) {
     return `${humanChoice}`;
   } else {
-    alert('This is not an option')
+    alert("This is not an option");
     return humanChoice;
   }
 }
@@ -38,400 +39,27 @@ function getHumanChoice() {
 const humanChoice = getHumanChoice();
 console.log(humanChoice);
 
-
-
-//SINGLE ROUND 
+//SINGLE ROUND
 
 function playRound(humanChoice, computerChoice) {
- 
-  if (humanChoice === 'rock' && computerChoice === 'scissors'||
-     humanChoice === 'paper' && computerChoice === 'rock' ||
-     humanChoice === 'scissors' && computerChoice === 'paper') {
-      return `You win ${humanChoice} beats ${computerChoice}`; 
-     }  else if (humanChoice === 'scissors' && computerChoice === 'rock'||
-      humanChoice === 'rock' && computerChoice === 'paper' ||
-      humanChoice === 'paper' && computerChoice === 'scissors' ) {
-        return `You lose ${computerChoice} beats ${humanChoice}`;
-     } else {
-      return "it's a tie";
-     }
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(`You win ${humanChoice} beats ${computerChoice}`);
+  } else if (
+    (humanChoice === "scissors" && computerChoice === "rock") ||
+    (humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "scissors")
+  ) {
+    console.log(`You lose ${computerChoice} beats ${humanChoice}`);
+  } else {
+    console.log("it's a tie");
+  }
 }
 
-const humanSelection = getHumanChoice; 
-const computerSelection = getComputerChoice;
+playRound();
 
-playRound(humanSelection, computerSelection);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(getHumanChoice());
+console.log(getComputerChoice());
