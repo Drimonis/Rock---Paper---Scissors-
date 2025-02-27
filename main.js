0a9cb199c37cd1646d55c11c3448dc3c34ec2d32
Dream <drimonis@protonmail.com>
2025-02-27 20:17:59 +0200

    This is the final project.

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-let humanScore = 0;
-let computerScore = 0;
+
+
+
 
 // COMPUTER CHOICE
 function getComputerChoice() {
@@ -14,8 +15,8 @@ function getComputerChoice() {
   }
 }
 
-const computerChoice = getComputerChoice();
-console.log(computerChoice);
+//const computerChoice = getComputerChoice();
+//console.log(computerChoice);
 
 // HUMAN CHOICE
 
@@ -36,30 +37,78 @@ function getHumanChoice() {
   }
 }
 
-const humanChoice = getHumanChoice();
-console.log(humanChoice);
+//const humanChoice = getHumanChoice();
+//console.log(humanChoice);
 
 //SINGLE ROUND
 
-function playRound(humanChoice, computerChoice) {
-  if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")
-  ) {
-    console.log(`You win ${humanChoice} beats ${computerChoice}`);
-  } else if (
-    (humanChoice === "scissors" && computerChoice === "rock") ||
-    (humanChoice === "rock" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "scissors")
-  ) {
-    console.log(`You lose ${computerChoice} beats ${humanChoice}`);
+// Play 5 Rounds
+
+
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      humanScore++;
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+    } else if (
+      (humanChoice === "scissors" && computerChoice === "rock") ||
+      (humanChoice === "rock" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "scissors")
+    ) {
+      computerScore++;
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    } else {
+      console.log("It's a tie!");
+    }
+  }
+
+  // Round 1
+  console.log("Round 1:");
+  const humanChoice1 = getHumanChoice();
+  const computerChoice1 = getComputerChoice();
+  playRound(humanChoice1, computerChoice1);
+
+  // Round 2
+  console.log("Round 2:");
+  const humanChoice2 = getHumanChoice();
+  const computerChoice2 = getComputerChoice();
+  playRound(humanChoice2, computerChoice2);
+
+  // Round 3
+  console.log("Round 3:");
+  const humanChoice3 = getHumanChoice();
+  const computerChoice3 = getComputerChoice();
+  playRound(humanChoice3, computerChoice3);
+
+  // Round 4
+  console.log("Round 4:");
+  const humanChoice4 = getHumanChoice();
+  const computerChoice4 = getComputerChoice();
+  playRound(humanChoice4, computerChoice4);
+
+  // Round 5
+  console.log("Round 5:");
+  const humanChoice5 = getHumanChoice();
+  const computerChoice5 = getComputerChoice();
+  playRound(humanChoice5, computerChoice5);
+
+  // Final Scores
+  console.log(`Final Scores - Human: ${humanScore}, Computer: ${computerScore}`);
+  if (humanScore > computerScore) {
+    console.log("You win the game!");
+  } else if (computerScore > humanScore) {
+    console.log("You lose the game!");
   } else {
-    console.log("it's a tie");
+    console.log("The game is a tie!");
   }
 }
 
-playRound();
-
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+playGame();
